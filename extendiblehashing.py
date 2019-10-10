f8ee7baf3a962659fea591fee7f52ee1e641bbd9
ABHIJOY SARKAR <asterdan712@gmail.com>
2019-10-10 16:33:49 +0530

    Some bug removal done

--- extendiblehashing.py
+++ extendiblehashing.py
@@ -5,9 +5,17 @@ def generate_data(lis_size,file_name):
     lis = synthesizer.generate_dataset([],lis_size)
     synthesizer.write_to_file(lis,file_name)
 
-choice = int(input("\nEnter a number: "))
 while(1):
+    print("\nEnter A Choice: ")
+    print("1. Generate Data")
+    print("2. Simulate Secondary Memory")
+    print("3. Insert Record")
+    print("4. Visualize Extendible hash")
+    choice = int(input())
+
     if choice == 1:
         generate_data(10,'dataset.txt') #passing list size, file name
-    elif choice ==2:
-        simulated_secondary_memory.simulate_secondary_memory('dataset.txt',3)
+    elif choice == 2:
+        alpha = int(input("\nEnter a block size: "))
+        simulated_secondary_memory.simulate_secondary_memory('dataset.txt',alpha)
+    
