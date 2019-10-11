a2fb66acb3ca4c6bd27d1c04c6898658c1eb4fc4
ABHIJOY SARKAR <asterdan712@gmail.com>
2019-10-11 18:51:36 +0530

    Initializations Fixed

--- extendiblehashing.py
+++ extendiblehashing.py
@@ -9,23 +9,19 @@ def generate_data(lis_size,file_name):
     synthesizer.write_to_file(lis,file_name)
 
 """ Entire operation needs to be put in a function at least """
-bucket_list = []    # list of buckets
+empty_spaces = 3
+bucket_list = [] # list of buckets
+bucket_list.append(Bucket(local_depth = 1, index_records = [], empty_spaces = empty_spaces))
+bucket_list.append(Bucket(local_depth = 1, index_records = [], empty_spaces = empty_spaces))   
+
 directory_records = [] # list of hash prefix with bucket pointers
+directory_records.append(DirectoryRecord(hash_prefix = "0", value = bucket_list[0]))
+directory_records.append(DirectoryRecord(hash_prefix = "1", value = bucket_list[1]))
+
+# Declaring the directory here
+directory = Directory(global_depth = 1, directory_records = directory_records) # Directory initialized here with global depth 0
 
 #%%
-# Declaring the directory here
-directory = Directory(global_depth = 0, directory_records = directory_records) # Directory initialized here with global depth 0
-j = 1 # Name of first disk block
-
-""" Complete this part after writing insert() """
-""" file handling for bulkloading done here """
-with open(str(j)+'.txt','r') as fin:
-    for line in fin:
-        line_modified = line[1:].rstrip(']\n').split(', ')
-        line_modified = [int(line_modified[i]) if i!=1 else line_modified[i].strip("\'") for i in range(len(line_modified))]
-        # I have a record properly stored in a list in line_modified
-        # call insert() for all records
-    
 """ insert function """    
 def insert(directory,index_record):
     # 1. Extract TID
@@ -35,22 +31,23 @@ def insert(directory,index_record):
     # 3. Extract global depth number of MSB given in directory.global_depth
     hash_prefix = TID_binary[:directory.global_depth]
     # 4. IndexRecord to be stored in a bucket
-    if bucket_list == []:
-        bucket = Bucket(local_depth = 0,index_records = list(index_record),empty_spaces=2) # Bucket of size 3
-        bucket_list.append(bucket)
-    else:
-        # search using hash prefix in directory
-        key = directory.directory_records.index(hash_prefix)
-        # key is my key
-        # directory.directory_records[key].value is a bucket
-        bucket = directory.directory_records[key].value
-        bucket.index_records.append(index_record)
-        bucket.empty_spaces=bucket.empty_spaces - 1
-        if bucket.empty_spaces < 0:
-            # Overflow
-        else:
-            # All izz well
-            pass
+    # search using hash prefix in directory
+    key = directory.directory_records.index(hash_prefix)
+    # key is my key
+    # directory.directory_records[key].value is a bucket
+    
+
+""" Complete this part after writing insert() """
+""" file handling for bulkloading done here """
+for j in range(1,4):
+    with open(str(j)+'.txt','r') as fin:
+        for line in fin:
+            line_modified = line[1:].rstrip(']\n').split(', ')
+            line_modified = [int(line_modified[i]) if i!=1 else line_modified[i].strip("\'") for i in range(len(line_modified))]
+            # I have a record properly stored in a list in line_modified
+            # call insert() for all records
+            index_record = [line_modified[0],str(j)+'.txt']
+            insert(directory,index_record)
 
 #%%
     
